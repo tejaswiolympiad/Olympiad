1a1e937927fa806cba45dc62fd230562898a070e
jitendra1000 <chauhanjitendra352@gmail.com>
2017-05-06 17:33:44 +0530

    modification

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Olympiad</title>
+	<title>Olympiad NGO</title>
   <link rel="stylesheet" href="{{ asset('public/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('public/css/login.css') }}" />  
   <script src="{{ asset('public/js/jquery.min.js') }}"></script>
