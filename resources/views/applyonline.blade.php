e38d2f641c576d20d5aa895164cf4f463055b444
jitendra1000 <chauhanjitendra352@gmail.com>
2017-05-07 20:17:58 +0530

    commit

--- resources/views/applyonline.blade.php
+++ resources/views/applyonline.blade.php
@@ -3,7 +3,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Olympiad</title>
+	<title>Olympiad NGO</title>
 	<link rel="stylesheet" href="{{ asset('public/css/bootstrap.min.css') }}" />
 	<link rel="stylesheet" href="{{ asset('public/css/applyonline.css') }}" />	
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
